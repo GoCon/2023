70480a4a73dace7b28659fb16b5acff1d712e4cd
S.Nakamatsu <19329+snaka@users.noreply.github.com>
2022-09-25 18:11:40 +0900

    button usage example

--- src/components/pages/PageTop/index.tsx
+++ src/components/pages/PageTop/index.tsx
@@ -1,10 +1,30 @@
+import { Box, Typography } from '@mui/material'
 import type { NextPage } from 'next'
+import { Button } from 'src/components/atoms'
 import { Layout } from 'src/components/commons'
+import { useTranslation } from 'react-i18next'
+import { useRouter } from 'next/router'
 
 export const PageTop: NextPage = () => {
+  const { t } = useTranslation()
+
   return (
     <Layout>
       implement later
+
+      <Typography variant="h2">
+        Wanted to Speakers!
+      </Typography>
+      <Box>
+        <Button text={t('apply_for_speaker')} onClick={ () => console.log('clicked!') }/>
+      </Box>
+
+      <Typography variant="h2">
+        Sponsors
+      </Typography>
+      <Box>
+        <Button text={t('consider_a_sponsor')} />
+      </Box>
     </Layout>
   )
 }
