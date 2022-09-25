ce4fc7c4fdb0f7c18faf2070b7fc11d16669ac20
maito1201 <itomasa0915@gmail.com>
2022-09-25 16:22:07 +0900

    feat: i18n

--- src/components/organisms/Footer/index.tsx
+++ src/components/organisms/Footer/index.tsx
@@ -1,22 +1,26 @@
 import { Box, Typography } from '@mui/material'
 import { Colors } from 'src/styles/color'
+import { useTranslation } from "react-i18next"
 
 export const footerHeight = 60
 
-export const Footer = () => (
-  <Box
-    sx={{
-      display: 'flex',
-      flexDirection: 'column-reverse',
-      alignItems: 'center',
-      width: '100%',
-      height: footerHeight,
-      backgroundColor: Colors.background.darken_1,
-      p: 1.5
-    }}
-  >
-    <Typography variant='caption' color={Colors.text.secondary}>
-      {`Gopherの原著作者はRenée Frenchさんです。 Original Creator of 'Gopher' is Renée French.`}
-    </Typography>
-  </Box>
-)
+export const Footer = () => {
+  const { t } = useTranslation()
+  return (
+    <Box
+      sx={{
+        display: 'flex',
+        flexDirection: 'column-reverse',
+        alignItems: 'center',
+        width: '100%',
+        height: footerHeight,
+        backgroundColor: Colors.background.darken_1,
+        p: 1.5
+      }}
+    >
+      <Typography variant='caption' color={Colors.text.secondary}>
+        {t('gopher_copyright')}
+      </Typography>
+    </Box>
+  )
+}
--- src/components/organisms/Header/index.tsx
+++ src/components/organisms/Header/index.tsx
@@ -1,25 +1,48 @@
 import React from 'react'
-import { AppBar, Container, Toolbar, Typography } from '@mui/material'
+import i18n from 'src/i18n/configs'
+import { AppBar, Toolbar, Typography, Box } from '@mui/material'
 import { Colors } from 'src/styles/color'
+import { useTranslation } from "react-i18next"
 
+const handleChangeLanguage = () => {
+  switch (i18n.language) {
+    case 'ja':
+      i18n.changeLanguage('en')
+      return
+    case 'en':
+      i18n.changeLanguage('ja')
+      return
+  }
+}
+
+// TODO: デザイン作る
 export const Header = () => {
+  const { t } = useTranslation()
+
   return (
     <AppBar position='static' sx={{ backgroundColor: Colors.background.gopher_blue }}>
       <Toolbar disableGutters sx={{ marginLeft: '10px' }}>
-        <Typography
-          variant='h6'
-          sx={{
-            mr: 2,
-            display: { xs: 'none', md: 'flex' },
-            fontFamily: 'monospace',
-            fontWeight: 700,
-            letterSpacing: '.3rem',
-            color: 'inherit',
-            textDecoration: 'none',
-          }}
-        >
-          Go Conference 2023
-        </Typography>
+        <Box>
+          <Typography
+            variant='h6'
+            sx={{
+              mr: 2,
+              display: { xs: 'none', md: 'flex' },
+              fontFamily: 'monospace',
+              fontWeight: 700,
+              letterSpacing: '.3rem',
+              color: 'inherit',
+              textDecoration: 'none',
+            }}
+          >
+            Go Conference 2023
+          </Typography>
+        </Box>
+        <Box sx={{margin: '0 10px 0 auto'}}>
+          <Typography onClick={handleChangeLanguage}>
+            {t('change_language')}
+          </Typography>
+        </Box>
       </Toolbar>
     </AppBar>
   )
