b1d785a968dda1e52aaf46f2636b324fcc726095
S.Nakamatsu <19329+snaka@users.noreply.github.com>
2022-09-25 17:45:16 +0900

    feat: Button component

---
+++ src/components/atoms/Button/index.tsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import { ButtonBase, Typography } from '@mui/material'
+import { Colors } from 'src/styles/color'
+
+type ButtonProps = {
+  text: string
+  width?: string | number
+  onClick?: () => void
+}
+
+export const Button = ({ text, width='370px', onClick }: ButtonProps ) => {
+  return (
+    <ButtonBase
+      sx={{
+        hight: '54px',
+        width,
+        borderRadius: '100px',
+        background: Colors.button.primary.enabled,
+        px: '24px',
+        py: '16px'
+      }}
+      onClick={onClick}
+    >
+      <Typography variant="body1" sx={{ color: Colors.button.primary.text }}>{text}</Typography>
+    </ButtonBase>
+  )
+}
