dcab32b881dabc5ab45569ccea69260643dc0d20
maito1201 <itomasa0915@gmail.com>
2022-09-25 18:21:36 +0900

    feat: header design

--- src/components/organisms/Header/index.tsx
+++ src/components/organisms/Header/index.tsx
@@ -3,6 +3,8 @@ import i18n from 'src/i18n/configs'
 import { AppBar, Toolbar, Typography, Box } from '@mui/material'
 import { Colors } from 'src/styles/color'
 import { useTranslation } from "react-i18next"
+import { Logo } from 'src/components/atoms'
+import { useScrollY } from 'src/modules/common/hooks'
 
 const handleChangeLanguage = () => {
   switch (i18n.language) {
@@ -15,31 +17,23 @@ const handleChangeLanguage = () => {
   }
 }
 
-// TODO: デザイン作る
+// TODO: 各ページへのリンクを実装, スマートフォンモード実装
 export const Header = () => {
+  const isScrolled = useScrollY() > 0
+  const fontColor = isScrolled ? "white" : "black"
   const { t } = useTranslation()
 
   return (
-    <AppBar position='static' sx={{ backgroundColor: Colors.background.gopher_blue }}>
-      <Toolbar disableGutters sx={{ marginLeft: '10px' }}>
+    <AppBar position="fixed" sx={{ backgroundColor: "#fff0", height: "100px", boxShadow: 0 }}>
+      {/* グラデーション背景 */}
+      <Box sx={{ transition: "200ms all linear", position: "absolute", width: "100%", height: "128px", background: Colors.background.gradation_blue, opacity: isScrolled ? 1 : 0 }} />
+
+      <Toolbar disableGutters >
         <Box>
-          <Typography
-            variant='h6'
-            sx={{
-              mr: 2,
-              display: { xs: 'none', md: 'flex' },
-              fontFamily: 'monospace',
-              fontWeight: 700,
-              letterSpacing: '.3rem',
-              color: 'inherit',
-              textDecoration: 'none',
-            }}
-          >
-            Go Conference 2023
-          </Typography>
+          <Logo sx={{ width: "233px", height: "40px", color: fontColor, marginLeft: "12px" }}/>
         </Box>
-        <Box sx={{margin: '0 10px 0 auto'}}>
-          <Typography onClick={handleChangeLanguage}>
+        <Box sx={{margin: '0 24px 0 auto'}}>
+          <Typography onClick={handleChangeLanguage} sx={{ color: fontColor, fontFamily: "Poppins" }}>
             {t('change_language')}
           </Typography>
         </Box>
