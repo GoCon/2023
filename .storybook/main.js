3bd813cd4e938a7184667bc4d14ea5808ce01d75
taigakiyokawa <kiyokawa.taiga@moneyforward.co.jp>
2022-10-03 22:52:17 +0900

    [chore] Add setup for Storybook to follow mui and resolve path
    And remove mdx, js, jsx, ts from target files for stories
    
    ref: https://zenn.dev/enish/articles/ff678649ecb6d9#2.-.storybook%E3%81%AE%E4%B8%AD%E3%81%AE%E3%83%95%E3%82%A1%E3%82%A4%E3%83%AB%E3%81%AE%E6%95%B4%E5%82%99

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,15 +1,18 @@
+const path = require('path')
+
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
-  ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions"
-  ],
-  "framework": "@storybook/react",
-  "core": {
-    "builder": "@storybook/builder-webpack5"
+  stories: ['../src/**/*.stories.tsx'],
+  addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-interactions'],
+  framework: '@storybook/react',
+  core: {
+    builder: '@storybook/builder-webpack5'
+  },
+  webpackFinal(config) {
+    config.resolve.modules = [...(config.resolve.modules || []), path.resolve(__dirname, '..')]
+
+    delete config.resolve.alias['emotion-theming']
+    delete config.resolve.alias['@emotion/styled']
+    delete config.resolve.alias['@emotion/core']
+    return config
   }
 }
