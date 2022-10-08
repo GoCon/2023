64339be16b4c1906d14cb3ef454730b7538bb19a
ahogappa <ahogappa@gmail.com>
2022-10-09 02:49:20 +0900

    add Montserrat to font family

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -8,6 +8,10 @@ export default function Document() {
           href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;300;400;500;700;900&display=swap"
           rel="stylesheet"
         />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100;300;400;500;700;900&display=swap"
+          rel="stylesheet"
+        />
       </Head>
       <body>
         <Main />
