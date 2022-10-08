7ce1d74878487da33678e322661f550c0483006d
ahogappa <ahogappa@gmail.com>
2022-10-09 05:01:56 +0900

    ガワだけ実装

---
+++ src/components/organisms/MainVisual/index.tsx
@@ -0,0 +1,8 @@
+import { Box, Container, Typography } from '@mui/material'
+import { Colors } from 'src/styles/color'
+import { useSize } from 'src/modules/common/hooks'
+import Image from 'next/image'
+
+export const MainVisual = () => {
+  return <></>
+}
--- src/components/pages/PageTop/index.tsx
+++ src/components/pages/PageTop/index.tsx
@@ -3,6 +3,7 @@ import type { NextPage } from 'next'
 import { Button } from 'src/components/atoms'
 import { Layout } from 'src/components/commons'
 import { useTranslation } from 'react-i18next'
+import { MainVisual } from 'src/components/organisms/MainVisual'
 import { TopDescription } from 'src/components/organisms/TopDescription'
 
 export const PageTop: NextPage = () => {
@@ -11,6 +12,7 @@ export const PageTop: NextPage = () => {
   return (
     <Layout>
       implement later
+      <MainVisual />
       <Typography variant="h2">Wanted to Speakers!</Typography>
       <Box>
         <Button text={t('apply_for_speaker')} onClick={() => console.log('clicked!')} />
