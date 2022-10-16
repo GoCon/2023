1ded53539398827efc93df4c8da4fffea4f58990
S.Nakamatsu <19329+snaka@users.noreply.github.com>
2022-10-16 17:35:54 +0900

    add colors

--- src/styles/color.ts
+++ src/styles/color.ts
@@ -2,6 +2,7 @@ export const Colors = {
   text: {
     primary: '#252A3A',
     secondary: '#636363',
+    secondary_default: '#666666',
     white: '#FFFFFF',
     default: '#333333',
     link: '#0078F9',
@@ -17,8 +18,14 @@ export const Colors = {
   },
   background: {
     primary: '#4D94E6', // Brand color
+    secondary: '#F7F9FB',
     gopher_blue: '#00ADD8',
     gradation_blue: 'linear-gradient(180deg, #4E96E9 19.27%, rgba(77, 148, 230, 0) 88.54%)',
     darken_1: 'rgba(0,0,0,0.125)'
+  },
+  border: {
+    primary: {
+      highlight: '#E5E5E5'
+    }
   }
 }
