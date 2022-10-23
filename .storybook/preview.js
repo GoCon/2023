ce2779c446b73c55826106602384728f6a7e52b1
taigakiyokawa <kiyokawa.taiga@moneyforward.co.jp>
2022-10-23 22:13:13 +0900

    [chore] Apply global styles on Storybook

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,22 +1,23 @@
 import { theme } from '../src/styles/theme'
 import { ThemeProvider } from '@mui/material/styles'
+import '../src/styles/globals.css'
 
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
+  actions: { argTypesRegex: '^on[A-Z].*' },
   controls: {
     matchers: {
       color: /(background|color)$/i,
-      date: /Date$/,
-    },
-  },
+      date: /Date$/
+    }
+  }
 }
 
 export const decorators = [
-    (Story) => {
-      return (
-        <ThemeProvider theme={theme}>
-          <Story />
-        </ThemeProvider>
-      )
-    },
+  Story => {
+    return (
+      <ThemeProvider theme={theme}>
+        <Story />
+      </ThemeProvider>
+    )
+  }
 ]
