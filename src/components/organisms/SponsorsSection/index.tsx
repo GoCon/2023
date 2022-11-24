8b2116ef543806e95906ffc143ee02e74422402a
taigakiyokawa <kiyokawa.taiga@moneyforward.co.jp>
2022-11-24 21:58:49 +0900

    [chore] Hide SponsorsCard until the top level sponsors has fixed

--- src/components/organisms/SponsorsSection/index.tsx
+++ src/components/organisms/SponsorsSection/index.tsx
@@ -1,15 +1,8 @@
 import type { FC } from 'react'
 import { Box, Typography } from '@mui/material'
-import { SponsorsCard } from 'src/components/molecules/SponsorsCard'
 import { Button } from 'src/components/atoms'
 import { useTranslation } from 'react-i18next'
 import { Colors } from 'src/styles/color'
-import SponsorsLogoSample from 'src/components/molecules/SponsorsCard/images/SponsorsLogoSample.png'
-
-const sampleLogo = {
-  image: SponsorsLogoSample,
-  alt: 'Logo Sample'
-}
 
 export const SponsorsSection: FC = () => {
   const { t } = useTranslation()
@@ -26,34 +19,13 @@ export const SponsorsSection: FC = () => {
       <Typography variant="h1" mb={5} textAlign={'center'}>
         Sponsors
       </Typography>
+      {/* NOTE: Hide SponsorsCard until the top level sponsors has fixed.
       <Box gap={3} mb={5} display={'flex'} flexDirection={'column'} width={'100%'} alignItems={'center'}>
-        <SponsorsCard planType="platinumGold" logoImages={[sampleLogo, sampleLogo]} />
-        <SponsorsCard planType="gold" logoImages={[sampleLogo, sampleLogo]} />
-        <SponsorsCard
-          planType="silver"
-          logoImages={[sampleLogo, sampleLogo, sampleLogo, sampleLogo, sampleLogo, sampleLogo]}
-        />
-        <SponsorsCard
-          planType="bronze"
-          logoImages={[
-            sampleLogo,
-            sampleLogo,
-            sampleLogo,
-            sampleLogo,
-            sampleLogo,
-            sampleLogo,
-            sampleLogo,
-            sampleLogo,
-            sampleLogo,
-            sampleLogo,
-            sampleLogo,
-            sampleLogo,
-            sampleLogo,
-            sampleLogo,
-            sampleLogo
-          ]}
-        />
-      </Box>
+        <SponsorsCard planType="platinumGold" logoImages={[]} />
+        <SponsorsCard planType="gold" logoImages={[]} />
+        <SponsorsCard planType="silver" logoImages={[]}/>
+        <SponsorsCard planType="bronze" logoImages={[]}/>
+      </Box> */}
       <Box mb={10}>
         <Button text={t('consider_a_sponsor')} />
       </Box>
