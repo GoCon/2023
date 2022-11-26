f6f4ac1b38d6924b70d84fbd681f07ab01e7e58c
taigakiyokawa <kiyokawa.taiga@moneyforward.co.jp>
2022-11-26 15:12:08 +0900

    [feat] Add illustrations

--- src/components/organisms/SpeakersSection/index.tsx
+++ src/components/organisms/SpeakersSection/index.tsx
@@ -4,6 +4,16 @@ import { Box, Typography } from '@mui/material'
 import { Button } from 'src/components/atoms'
 import { useTranslation } from 'react-i18next'
 import { Colors } from 'src/styles/color'
+import {
+  GopherConductor,
+  GopherDrummer,
+  GopherFlowerBlue,
+  GopherFlowerPink,
+  GopherPartyPopper,
+  GopherPomPom,
+  GopherTrumpeter
+} from 'src/assets/gopher'
+import Image from 'next/image'
 
 export const SpeakersSection: FC = () => {
   const { t } = useTranslation()
@@ -23,11 +33,24 @@ export const SpeakersSection: FC = () => {
         <Typography>{t('application_started')}: 2022.12.01 Thu</Typography>
         <Typography>{t('application_closed')}: 2023.01.31 Sat</Typography>
       </Box>
-      <Link href="https://sessionize.com/go-conference-2023-online/">
-        <a target="_blank">
-          <Button text={t('apply_for_speaker')} />
-        </a>
-      </Link>
+      <Box display="grid" gridTemplateColumns="1fr 1fr 1fr" gap={2}>
+        <Box display="flex" alignItems="flex-end" justifyContent="flex-end" gap={0.5}>
+          <Image src={GopherConductor} alt="gopher conductor" />
+          <Image src={GopherDrummer} alt="gopher drummer" />
+          <Image src={GopherTrumpeter} alt="gopher trumpeter" />
+          <Image src={GopherPomPom} alt="gopher pom pom" />
+        </Box>
+        <Link href="https://sessionize.com/go-conference-2023-online/">
+          <a target="_blank">
+            <Button text={t('apply_for_speaker')} />
+          </a>
+        </Link>
+        <Box display="flex" alignItems="flex-end" gap={0.5}>
+          <Image src={GopherFlowerBlue} alt="gopher flower blue" />
+          <Image src={GopherFlowerPink} alt="gopher flower pink" />
+          <Image src={GopherPartyPopper} alt="gopher party popper" />
+        </Box>
+      </Box>
     </Box>
   )
 }
