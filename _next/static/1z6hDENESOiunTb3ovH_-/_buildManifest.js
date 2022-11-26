5ca0dce31e2badaf8a8d5cd0df4bd8d8fc090237
taigakiyokawa <taigakiyokawa@users.noreply.github.com>
2022-11-26 09:51:09 +0000

    deploy: 0e153c47e55179482593d0990f6ba02832be1dc2

--- _next/static/1z6hDENESOiunTb3ovH_-/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/fec483df-c8cf40203e53dd6e.js","static/chunks/386-4f94e16946edb05f.js","static/chunks/pages/index-d57a0f3f0472b120.js"],"/_error":["static/chunks/pages/_error-e4f561a102d9bb14.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
