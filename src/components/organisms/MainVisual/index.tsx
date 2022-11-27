498b7f1bc96db34f110337e124485a293d6c3193
taigakiyokawa <kiyokawa.taiga@moneyforward.co.jp>
2022-11-27 13:37:06 +0900

    [feat] Shot off confetti when the component has rendered on bottom left and right side of MainVisual

--- src/components/organisms/MainVisual/index.tsx
+++ src/components/organisms/MainVisual/index.tsx
@@ -1,13 +1,26 @@
 import { Box } from '@mui/material'
 import { useSize } from 'src/modules/hooks'
+import { useReward } from 'react-rewards'
+import { useEffect } from 'react'
 
 export const MainVisual = () => {
   const { isTabletOrOver } = useSize()
   const image_path = isTabletOrOver ? './Background-pc.png' : './Background-sp.png'
+  const { reward: rewardLeft } = useReward('confettiLeft', 'confetti')
+  const { reward: rewardRight } = useReward('confettiRight', 'confetti')
+
+  useEffect(() => {
+    rewardLeft()
+    rewardRight()
+  }, [rewardLeft, rewardRight])
 
   return (
     <Box sx={{ maxWidth: '1440px', margin: '0 auto' }}>
       <img src={image_path} alt="main visual" style={{ width: '100%' }} />
+      <Box sx={{ display: 'flex', justifyContent: 'space-between' }}>
+        <span id="confettiLeft" />
+        <span id="confettiRight" />
+      </Box>
     </Box>
   )
 }
