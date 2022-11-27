8751b42da8b2b9a272424267c1572a04334418ea
taigakiyokawa <kiyokawa.taiga@moneyforward.co.jp>
2022-11-27 15:01:34 +0900

    [feat] Add additional confetti on top-left and right

--- src/components/organisms/MainVisual/index.tsx
+++ src/components/organisms/MainVisual/index.tsx
@@ -5,25 +5,43 @@ import { useReward } from 'react-rewards'
 export const MainVisual = () => {
   const { isTabletOrOver } = useSize()
   const image_path = isTabletOrOver ? './Background-pc.png' : './Background-sp.png'
-  const { reward: rewardLeft, isAnimating: isAnimatingLeft } = useReward('confettiLeft', 'confetti', { angle: 60 })
-  const { reward: rewardRight, isAnimating: isAnimatingRight } = useReward('confettiRight', 'confetti', { angle: 120 })
+  const { reward: rewardTopLeft, isAnimating: isAnimatingTopLeft } = useReward('confettiTopLeft', 'confetti', {
+    angle: 45
+  })
+  const { reward: rewardTopRight, isAnimating: isAnimatingTopRight } = useReward('confettiTopRight', 'confetti', {
+    angle: 135
+  })
+  const { reward: rewardBottomLeft, isAnimating: isAnimatingBottomLeft } = useReward('confettiBottomLeft', 'confetti', {
+    angle: 45
+  })
+  const { reward: rewardBottomRight, isAnimating: isAnimatingBottomRight } = useReward(
+    'confettiBottomRight',
+    'confetti',
+    { angle: 135 }
+  )
 
   useInterval(() => {
-    if (!isAnimatingLeft || !isAnimatingRight) {
-      rewardLeft()
-      rewardRight()
+    if (!isAnimatingTopLeft || !isAnimatingTopRight || !isAnimatingBottomLeft || !isAnimatingBottomRight) {
+      rewardTopLeft()
+      rewardTopRight()
+      rewardBottomLeft()
+      rewardBottomRight()
     }
   })
 
   return (
-    <>
-      <Box sx={{ maxWidth: '1440px', margin: '0 auto' }}>
-        <img src={image_path} alt="main visual" style={{ width: '100%' }} />
+    <Box sx={{ width: '100%' }}>
+      <Box sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
+        <span id="confettiTopLeft" />
+        <Box sx={{ maxWidth: '1440px', margin: '0 auto' }}>
+          <img src={image_path} alt="main visual" style={{ width: '100%' }} />
+        </Box>
+        <span id="confettiTopRight" />
       </Box>
       <Box sx={{ display: 'flex', justifyContent: 'space-between' }}>
-        <span id="confettiLeft" />
-        <span id="confettiRight" />
+        <span id="confettiBottomLeft" />
+        <span id="confettiBottomRight" />
       </Box>
-    </>
+    </Box>
   )
 }
