182afcc1160ea96364873c3ca702ff073844696c
Taiga KIYOKAWA <40013676+taigakiyokawa@users.noreply.github.com>
2022-11-27 17:54:11 +0900

    [feat] Shot off confetti on sides of main visual like party popper 🎉 (#54)
    * [chore] Add `react-rewards` to shot off confetti like fireworks
    
    * [feat] Shot off confetti when the component has rendered on bottom left and right side of MainVisual
    
    * [feat] Add `useInterval` to use `setInterval` with React lifecycle safety
    
    ref: https://zenn.dev/akhr_s/articles/065e18ab3c4883
    
    * [feat] Add additional confetti on top-left and right
    
    * [chore] Adjust config for confetti (colors, position, and elementCount)
    
    ref: https://github.com/thedevelobear/react-rewards#props--config
    
    * [chore] Adjust the interval of calling confetti functions
    
    * [chore] Move confettiColors to colors.ts
    
    * [easteregg] Make Gopher has party popper to shot off confetti on click

--- src/components/organisms/MainVisual/index.tsx
+++ src/components/organisms/MainVisual/index.tsx
@@ -1,13 +1,77 @@
 import { Box } from '@mui/material'
-import { useSize } from 'src/modules/hooks'
+import { useInterval, useSize } from 'src/modules/hooks'
+import { useReward } from 'react-rewards'
+import { confettiColors } from 'src/styles/color'
 
 export const MainVisual = () => {
   const { isTabletOrOver } = useSize()
   const image_path = isTabletOrOver ? './Background-pc.png' : './Background-sp.png'
+  const confettiLeftConfig = {
+    angle: 45,
+    colors: confettiColors,
+    elementCount: isTabletOrOver ? 100 : 50,
+    position: 'absolute'
+  }
+  const confettiRightConfig = {
+    angle: 135,
+    colors: confettiColors,
+    elementCount: isTabletOrOver ? 100 : 50,
+    position: 'absolute'
+  }
+  const { reward: rewardTopLeft, isAnimating: isAnimatingTopLeft } = useReward(
+    'confettiTopLeft',
+    'confetti',
+    confettiLeftConfig
+  )
+  const { reward: rewardTopRight, isAnimating: isAnimatingTopRight } = useReward(
+    'confettiTopRight',
+    'confetti',
+    confettiRightConfig
+  )
+  const { reward: rewardBottomLeft, isAnimating: isAnimatingBottomLeft } = useReward(
+    'confettiBottomLeft',
+    'confetti',
+    confettiLeftConfig
+  )
+  const { reward: rewardBottomRight, isAnimating: isAnimatingBottomRight } = useReward(
+    'confettiBottomRight',
+    'confetti',
+    confettiRightConfig
+  )
+
+  useInterval(
+    () => {
+      if (!isAnimatingTopLeft || !isAnimatingTopRight || !isAnimatingBottomLeft || !isAnimatingBottomRight) {
+        rewardTopLeft()
+        rewardTopRight()
+        rewardBottomLeft()
+        rewardBottomRight()
+      }
+    },
+    4000,
+    true // call the function immediately after rendered
+  )
 
   return (
-    <Box sx={{ maxWidth: '1440px', margin: '0 auto' }}>
-      <img src={image_path} alt="main visual" style={{ width: '100%' }} />
+    <Box sx={{ width: '100%' }}>
+      <Box
+        sx={{
+          position: 'relative',
+          display: 'flex',
+          justifyContent: 'space-between',
+          alignItems: 'center'
+        }}
+      >
+        <Box id="confettiTopLeft" sx={{ position: 'absolute', left: '32px' }} />
+        <Box sx={{ maxWidth: '1440px', margin: '0 auto' }}>
+          <img src={image_path} alt="main visual" style={{ width: '100%' }} />
+        </Box>
+        <Box id="confettiTopRight" sx={{ position: 'absolute', right: '32px' }} />
+      </Box>
+      <Box sx={{ position: 'relative' }}>
+        <Box id="confettiBottomLeft" sx={{ position: 'absolute', left: '32px' }} />
+        <Box id="confettiBottomRight" sx={{ position: 'absolute', right: '32px' }} />
+      </Box>
     </Box>
   )
 }
--- src/modules/hooks/index.ts
+++ src/modules/hooks/index.ts
@@ -1,6 +1,7 @@
 import { useMediaQuery } from '@mui/material'
 import { Theme } from '@mui/system'
 import { useSyncExternalStore } from 'react'
+import { useEffect, useRef } from 'react'
 
 export const useSize = () => {
   const isPCOrOver = useMediaQuery((theme: Theme) => theme.breakpoints.up('md'))
@@ -22,4 +23,20 @@ function subscribeScroll(callback: () => void): () => void {
   return () => {
     window.removeEventListener('scroll', callback)
   }
+}
+
+// ref: https://zenn.dev/akhr_s/articles/065e18ab3c4883
+export const useInterval = (callback: () => void, interval: number = 1000, callImmediately: boolean = false) => {
+  const callbackRef = useRef(() => {})
+
+  useEffect(() => {
+    callbackRef.current = callback
+  }, [callback])
+
+  useEffect(() => {
+    // call the function immediately after rendered
+    callImmediately && callbackRef.current()
+    const timerId = setInterval(() => callbackRef.current(), interval)
+    return () => clearInterval(timerId)
+  }, [interval, callImmediately])
 }
