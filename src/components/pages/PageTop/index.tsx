0c7d0111fed9eff65d69cb99bf1ba8a936745ebc
maito1201 <49754654+maito1201@users.noreply.github.com>
2023-02-19 23:09:12 +0900

    feat: add information about CfP result (#165)

--- src/components/pages/PageTop/index.tsx
+++ src/components/pages/PageTop/index.tsx
@@ -1,7 +1,13 @@
 import type { NextPage } from 'next'
 import { Layout } from 'src/components/commons'
 import { useTranslation } from 'react-i18next'
-import { MainVisual, TopDescription, SponsorsSection, CommunityBoothSection } from 'src/components/organisms'
+import {
+  MainVisual,
+  TopDescription,
+  SponsorsSection,
+  CommunityBoothSection,
+  SpeakersSection
+} from 'src/components/organisms'
 
 export const PageTop: NextPage = () => {
   const { t } = useTranslation()
@@ -10,6 +16,7 @@ export const PageTop: NextPage = () => {
     <Layout>
       <MainVisual />
       <TopDescription />
+      <SpeakersSection />
       <SponsorsSection />
       <CommunityBoothSection />
     </Layout>
