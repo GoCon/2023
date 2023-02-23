3d3ff1f3e7a06e08bd675c54efd7bea18416db32
maito1201 <49754654+maito1201@users.noreply.github.com>
2023-02-23 12:28:06 +0900

    Merge main into develop (#169)
    * [fix] Fix date for closing application from Sat to Tue (#65) (#66)
    
    * feat: community booth section (#67)
    
    * feat: specified commercial transaction (#71)
    
    * feat: specified commercial transaction
    
    * fix typo
    
    * fix: i18n locale
    
    * fix: english explanation
    
    * feat: add information about CfP result (#165)
    
    ---------
    
    Co-authored-by: Taiga KIYOKAWA <40013676+taigakiyokawa@users.noreply.github.com>

--- src/components/organisms/SpeakersSection/index.tsx
+++ src/components/organisms/SpeakersSection/index.tsx
@@ -4,16 +4,6 @@ import { Box, Typography } from '@mui/material'
 import { Button } from 'src/components/atoms'
 import { useTranslation } from 'react-i18next'
 import { Colors, confettiColors } from 'src/styles/color'
-import {
-  GopherConductor,
-  GopherDrummer,
-  GopherFlowerBlue,
-  GopherFlowerPink,
-  GopherPartyPopper,
-  GopherPomPom,
-  GopherTrumpeter
-} from 'src/images/gopher'
-import Image from 'next/image'
 import { useSize } from 'src/modules/hooks'
 import { useReward } from 'react-rewards'
 
@@ -38,50 +28,13 @@ export const SpeakersSection: FC = () => {
     >
       <Typography variant="h2">Call for Speakers!</Typography>
       <Box mb={1}>
-        <Typography variant="body2">{t('application_started')}: 2022.12.01 Thu</Typography>
-        <Typography variant="body2">{t('application_closed')}: 2023.01.31 Tue</Typography>
+        <Typography variant="body2">{t('application_result_will_be_noticed')}</Typography>
       </Box>
-      <Box display="grid" gridTemplateColumns={isTabletOrOver ? '1fr 1fr 1fr' : '1fr'} gap={2}>
-        {isTabletOrOver && (
-          <Box display="flex" alignItems="flex-end" justifyContent="flex-end" gap={0.5}>
-            <Image src={GopherConductor} alt="gopher conductor" />
-            <Image src={GopherDrummer} alt="gopher drummer" />
-            <Image src={GopherTrumpeter} alt="gopher trumpeter" />
-            <Image src={GopherPomPom} alt="gopher pom pom" />
-          </Box>
-        )}
-        <Link href="https://sessionize.com/go-conference-2023-online/">
-          <a target="_blank">
-            <Button text={t('apply_for_speaker')} />
-          </a>
-        </Link>
-        {isTabletOrOver && (
-          <Box display="flex" alignItems="flex-end" gap={0.5}>
-            <Image src={GopherFlowerBlue} alt="gopher flower blue" />
-            <Image src={GopherFlowerPink} alt="gopher flower pink" />
-            <Box onClick={reward} sx={{ '&:hover': { cursor: 'pointer' } }}>
-              <span id="confettiGopherPopper" />
-              <Image src={GopherPartyPopper} alt="gopher party popper" />
-            </Box>
-          </Box>
-        )}
-      </Box>
-      {!isTabletOrOver && (
-        <Box
-          sx={{
-            position: 'absolute',
-            zIndex: 0,
-            top: '56px',
-            right: 0,
-            display: 'flex',
-            alignItems: 'flex-end',
-            opacity: 0.7
-          }}
-        >
-          <Image src={GopherConductor} alt="gopher conductor" width="56px" objectFit="contain" />
-          <Image src={GopherDrummer} alt="gopher drummer" width="52px" objectFit="contain" />
-        </Box>
-      )}
+      <Link href="https://sessionize.com/go-conference-2023-online/">
+        <a target="_blank">
+          <Button text={t('check_cfp')} />
+        </a>
+      </Link>
     </Box>
   )
 }
