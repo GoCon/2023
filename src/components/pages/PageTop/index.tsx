3770f914005ba7f42a139f43e19e0531d031edcd
maito1201 <itomasa0915@gmail.com>
2023-02-23 16:38:13 +0900

    feat: useSettionize

--- src/components/pages/PageTop/index.tsx
+++ src/components/pages/PageTop/index.tsx
@@ -1,6 +1,7 @@
 import type { NextPage } from 'next'
 import { Layout } from 'src/components/commons'
 import { useTranslation } from 'react-i18next'
+import { useSessionize } from 'src/modules/sessionize/hooks'
 import {
   MainVisual,
   TopDescription,
@@ -11,6 +12,10 @@ import {
 
 export const PageTop: NextPage = () => {
   const { t } = useTranslation()
+
+  // TODO(@maito1201): 取得したデータを基にセッションデータを表示する
+  const { data } = useSessionize()
+  console.log('session data', data)
 
   return (
     <Layout>
