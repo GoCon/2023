c01d2b6ce5d889274bc347e2c8b94a134a9d1073
Shinji Nakamatsu <19329+snaka@users.noreply.github.com>
2023-02-26 10:14:26 +0900

    スポンサー(パートナー)ページの実装 (#170)
    * feat: partner page
    
    * SAMPLE json / images
    
    * fix: `desc` to `description`
    
    * fix: simplify prop type name
    
    * fix: `desc` to `description`
    
    * fix: extract params from context

---
+++ src/components/pages/PagePartner/index.tsx
@@ -0,0 +1,31 @@
+import { Box, Typography } from '@mui/material'
+import { NextPage } from 'next'
+import Image from 'next/image'
+import { Layout } from 'src/components/commons'
+import { Header } from 'src/components/organisms'
+
+export type PartnerRank = 'platinum' | 'gold'
+
+type Props = {
+  id: string
+  name: string
+  description: string
+}
+
+export const PagePartner = ({ id, name, description }: Props) => {
+  return (
+    <Layout>
+      <Box sx={{ mx: '160px' }} display="flex" flexDirection="column">
+        <Box sx={{ mt: '120px', py: '24px' }} display="flex">
+          <Image src={`/2023/images/partners/${id}.png`} width={'200px'} height={'120px'} alt={id} />
+          <Typography variant="h1" sx={{ ml: '24px', width: '100%', textAlign: 'center', alignContent: 'center' }}>
+            {name}
+          </Typography>
+        </Box>
+        <Box>
+          <Typography variant="body1">{description}</Typography>
+        </Box>
+      </Box>
+    </Layout>
+  )
+}
