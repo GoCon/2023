54c7b79a4eb2c873fe8e896f3c6147a8578dc1d3
taigakiyokawa <kiyokawa.taiga@moneyforward.co.jp>
2023-03-31 21:32:11 +0900

    chore: Remove `next/link` temporary for forwarding to HTML pages build by Sessionize

--- src/components/molecules/HeaderMenu/index.tsx
+++ src/components/molecules/HeaderMenu/index.tsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import { Typography, Box } from '@mui/material'
-import Link from 'next/link'
 import { useRouter } from 'next/router'
 import { HeaderItemColor, HeaderMenuItem, HeaderItemBehaviorStyles } from 'src/components/organisms/Header'
 
@@ -17,20 +16,19 @@ export const HeaderMenu = ({ menuList, itemColor, itemBehaviorStyles }: HeaderMe
     <Box sx={{ display: 'flex', gap: '8px', margin: '0 24px 0 auto' }}>
       {menuList.map((list, i) => {
         return list.href ? (
-          <Link href={list.href} key={i}>
-            <a>
-              <Typography
-                sx={{
-                  borderBottom: router.pathname === list.href ? '3px solid' : '',
-                  color: itemColor.default,
-                  p: '4px 8px',
-                  ...itemBehaviorStyles
-                }}
-              >
-                {list.label}
-              </Typography>
-            </a>
-          </Link>
+          // TODO(taigakiyokawa): Revert using `next/link` when pages have implemented.
+          <a href={list.href} target="_blank" rel="noreferrer">
+            <Typography
+              sx={{
+                borderBottom: router.pathname === list.href ? '3px solid' : '',
+                color: itemColor.default,
+                p: '4px 8px',
+                ...itemBehaviorStyles
+              }}
+            >
+              {list.label}
+            </Typography>
+          </a>
         ) : (
           <Typography
             onClick={list.onClick}
