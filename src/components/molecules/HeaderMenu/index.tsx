698ee86f1368b0845280e778bd85149c44c8060b
taigakiyokawa <kiyokawa.taiga@moneyforward.co.jp>
2023-04-01 10:57:24 +0900

    fix(HeaderMenu): Use `next/link` when href is `/`

--- src/components/molecules/HeaderMenu/index.tsx
+++ src/components/molecules/HeaderMenu/index.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { Typography, Box } from '@mui/material'
 import { useRouter } from 'next/router'
 import { HeaderItemColor, HeaderMenuItem, HeaderItemBehaviorStyles } from 'src/components/organisms/Header'
+import Link from 'next/link'
 
 export interface HeaderMenuProps {
   menuList: HeaderMenuItem[]
@@ -16,19 +17,36 @@ export const HeaderMenu = ({ menuList, itemColor, itemBehaviorStyles }: HeaderMe
     <Box sx={{ display: 'flex', gap: '8px', margin: '0 24px 0 auto' }}>
       {menuList.map((list, i) => {
         return list.href ? (
-          // TODO(taigakiyokawa): Revert using `next/link` when pages have implemented.
-          <a href={list.href} target="_blank" rel="noreferrer">
-            <Typography
-              sx={{
-                borderBottom: router.pathname === list.href ? '3px solid' : '',
-                color: itemColor.default,
-                p: '4px 8px',
-                ...itemBehaviorStyles
-              }}
-            >
-              {list.label}
-            </Typography>
-          </a>
+          list.href === '/' ? (
+            <Link href={list.href} key={i}>
+              <a>
+                <Typography
+                  sx={{
+                    borderBottom: router.pathname === list.href ? '3px solid' : '',
+                    color: itemColor.default,
+                    p: '4px 8px',
+                    ...itemBehaviorStyles
+                  }}
+                >
+                  {list.label}
+                </Typography>
+              </a>
+            </Link>
+          ) : (
+            // TODO(taigakiyokawa): Revert using `next/link` when pages have implemented.
+            <a href={list.href} target="_blank" rel="noreferrer" key={i}>
+              <Typography
+                sx={{
+                  borderBottom: router.pathname === list.href ? '3px solid' : '',
+                  color: itemColor.default,
+                  p: '4px 8px',
+                  ...itemBehaviorStyles
+                }}
+              >
+                {list.label}
+              </Typography>
+            </a>
+          )
         ) : (
           <Typography
             onClick={list.onClick}
