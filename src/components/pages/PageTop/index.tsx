6be83d37154dd89b496c336d6f26da536f1cb04b
maito1201 <49754654+maito1201@users.noreply.github.com>
2023-04-02 12:06:02 +0900

    fix: remove community booth section (#177)
    * feat: community booth section (#67)
    
    * fix: remove community booth section

--- src/components/pages/PageTop/index.tsx
+++ src/components/pages/PageTop/index.tsx
@@ -1,12 +1,9 @@
 import type { NextPage } from 'next'
 import { Layout } from 'src/components/commons'
-import { useTranslation } from 'react-i18next'
 // import { useSessionize } from 'src/modules/sessionize/hooks'
-import { MainVisual, TopDescription, SponsorsSection, CommunityBoothSection } from 'src/components/organisms'
+import { MainVisual, TopDescription, SponsorsSection } from 'src/components/organisms'
 
 export const PageTop: NextPage = () => {
-  const { t } = useTranslation()
-
   // TODO(@maito1201): 取得したデータを基にセッションデータを表示する
   // const { data } = useSessionize()
   // console.log('session data', data)
