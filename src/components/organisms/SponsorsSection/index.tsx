cb1cafe0f69829f0d0e03884c17a7e53c68c9535
Taiga KIYOKAWA <40013676+taigakiyokawa@users.noreply.github.com>
2023-04-02 13:19:53 +0900

    chore: Close sponsors application (#180)
    * chore: Remove the button for application to sponsors due to be already closed
    
    * chore: Fix arrangement of Gophers due to remove the button
    
    * chore: Change description to "Sponsors application has been closed.~"

--- src/components/organisms/SponsorsSection/index.tsx
+++ src/components/organisms/SponsorsSection/index.tsx
@@ -1,8 +1,6 @@
 import type { FC } from 'react'
-import Link from 'next/link'
 import { Box, Typography } from '@mui/material'
-import { Button } from 'src/components/atoms'
-import { useTranslation } from 'react-i18next'
+import { Trans, useTranslation } from 'react-i18next'
 import { Colors, confettiColors } from 'src/styles/color'
 import {
   GopherConductor,
@@ -40,9 +38,11 @@ export const SponsorsSection: FC = () => {
       <Typography variant="h2" textAlign={'center'}>
         Sponsors
       </Typography>
-      {/* TODO: This description will be changed as soon as the official wording is fixed. */}
-      <Typography variant="body1" mb={{ md: 5, xs: 2 }}>
-        {t('sponsors_description')}
+      <Typography
+        variant="body1"
+        sx={{ mb: { md: 5, xs: 2 }, textAlign: 'center', wordBreak: 'keep-all', overflowWrap: 'break-word' }}
+      >
+        <Trans t={t} i18nKey="sponsors_application_closed" />
       </Typography>
       {/* NOTE: Hide SponsorsCard until the top level sponsors has fixed.
       <Box gap={3} mb={5} display={'flex'} flexDirection={'column'} width={'100%'} alignItems={'center'}>
@@ -51,47 +51,22 @@ export const SponsorsSection: FC = () => {
         <SponsorsCard planType="silver" logoImages={[]}/>
         <SponsorsCard planType="bronze" logoImages={[]}/>
       </Box> */}
-      <Box display="grid" gridTemplateColumns={isTabletOrOver ? '1fr 1fr 1fr' : '1fr'} gap={2}>
+      <Box display="flex" alignItems="flex-end" justifyContent="flex-end" gap={0.5}>
+        <Image src={GopherConductor} alt="gopher conductor" />
+        <Image src={GopherDrummer} alt="gopher drummer" />
+        <Image src={GopherTrumpeter} alt="gopher trumpeter" />
+        <Image src={GopherPomPom} alt="gopher pom pom" />
+        <Image src={GopherFlowerBlue} alt="gopher flower blue" />
         {isTabletOrOver && (
-          <Box display="flex" alignItems="flex-end" justifyContent="flex-end" gap={0.5}>
-            <Image src={GopherConductor} alt="gopher conductor" />
-            <Image src={GopherDrummer} alt="gopher drummer" />
-            <Image src={GopherTrumpeter} alt="gopher trumpeter" />
-            <Image src={GopherPomPom} alt="gopher pom pom" />
-          </Box>
-        )}
-        <Link href="https://drive.google.com/file/d/1wwFeJk0rT0SydwDi2wx4wVVAD6psDUrL/view?usp=share_link">
-          <a target="_blank">
-            <Button text={t('consider_a_sponsor')} />
-          </a>
-        </Link>
-        {isTabletOrOver && (
-          <Box display="flex" alignItems="flex-end" gap={0.5}>
-            <Image src={GopherFlowerBlue} alt="gopher flower blue" />
+          <>
             <Image src={GopherFlowerPink} alt="gopher flower pink" />
             <Box onClick={reward} sx={{ '&:hover': { cursor: 'pointer' } }}>
               <span id="confettiGopherPopper" />
               <Image src={GopherPartyPopper} alt="gopher party popper" />
             </Box>
-          </Box>
+          </>
         )}
       </Box>
-      {!isTabletOrOver && (
-        <Box
-          sx={{
-            position: 'absolute',
-            zIndex: 0,
-            top: '56px',
-            right: 0,
-            display: 'flex',
-            alignItems: 'flex-end',
-            opacity: 0.7
-          }}
-        >
-          <Image src={GopherConductor} alt="gopher conductor" width="56px" objectFit="contain" />
-          <Image src={GopherDrummer} alt="gopher drummer" width="52px" objectFit="contain" />
-        </Box>
-      )}
     </Box>
   )
 }
