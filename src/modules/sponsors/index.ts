3e45d92fb46cdffa3904bf3d42bd53b0672c5686
taigakiyokawa <kiyokawa.taiga@moneyforward.co.jp>
2023-05-11 19:35:48 +0900

    feat: Add GO's logo

--- src/modules/sponsors/index.ts
+++ src/modules/sponsors/index.ts
@@ -9,7 +9,7 @@ export type SponsorInfo = {
 }
 
 const platinumSponsors: SponsorInfo[] = [
-  // { id: 1, name: 'GO株式会社', logo: 'sponsors/platinum/01_go.png' },
+  { id: 1, name: 'GO株式会社', logo: 'sponsors/platinum/01_go.png' },
   { id: 2, name: '株式会社カンム', logo: 'sponsors/platinum/02_kanmu.png' }
 ]
 
