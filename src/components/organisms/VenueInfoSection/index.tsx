63c9d294ccb5e09e90219a4d6bfd8a4b7e420a8a
maito1201 <49754654+maito1201@users.noreply.github.com>
2023-05-22 22:06:16 +0900

    Update src/components/organisms/VenueInfoSection/index.tsx
    Co-authored-by: Taiga KIYOKAWA <40013676+taigakiyokawa@users.noreply.github.com>

--- src/components/organisms/VenueInfoSection/index.tsx
+++ src/components/organisms/VenueInfoSection/index.tsx
@@ -10,9 +10,7 @@ export const VenueInfoSection = () => {
       <Box mb="32px" width="320px" height="140px" position="relative" display="flex" alignItems="center">
         <Image src={FloorImage} alt="floor map" fill style={{ objectFit: 'contain', borderRadius: '8px' }} />
       </Box>
-      <Box mb="24px" maxWidth="680px">
-        <Typography variant="body2">{t('floor_guide')}</Typography>
-      </Box>
+      <Typography variant="body2" sx={{ mb: '24px', maxWidth: '680px' }}>{t('floor_guide')}</Typography>
       <Box>
         {/* TODO: reBakoのリンクを実装する
           <a href="#" target="_blank">
