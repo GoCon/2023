f7b1a39141a3bdf28ff23acf53d459f46e810e5d
maito1201 <49754654+maito1201@users.noreply.github.com>
2023-05-22 22:05:24 +0900

    Update src/components/molecules/BoothCard/index.tsx
    Co-authored-by: Taiga KIYOKAWA <40013676+taigakiyokawa@users.noreply.github.com>

--- src/components/molecules/BoothCard/index.tsx
+++ src/components/molecules/BoothCard/index.tsx
@@ -8,9 +8,7 @@ export interface BoothCardProps {
 export const BoothCard = ({ title, description }: BoothCardProps) => {
   return (
     <Box display={'flex'} flexDirection={'column'} alignItems={'center'} py={{ md: 4, xs: 4 }}>
-      <Box>
-        <Typography variant="h3">{title}</Typography>
-      </Box>
+      <Typography variant="h3">{title}</Typography>
       <Box mb="24px" maxWidth="680px">
         <Typography variant="body2" sx={{ wordBreak: 'break-word', whiteSpace: 'pre-wrap' }}>
           {description}
