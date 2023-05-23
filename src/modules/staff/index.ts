ebfb942becb0bfa4ba5d7c4f133a9cf0ce61fd57
taigakiyokawa <kiyokawa.taiga@moneyforward.co.jp>
2023-05-23 20:44:11 +0900

    feat: Add staff information list

---
+++ src/modules/staff/index.ts
@@ -0,0 +1,121 @@
+/**
+ * Each staff information
+ */
+export type StaffInfo = {
+  name: string
+  icon: string
+  organizationName?: string
+  twitterLink?: string
+  githubLink?: string
+  otherLink?: string
+}
+
+/**
+ * Staff information list used in `/staff` page.
+ */
+export const staff: StaffInfo[] = [
+  {
+    name: '山口能迪',
+    organizationName: 'グーグル合同会社',
+    icon: 'https://pbs.twimg.com/profile_images/1384005345454936073/Jw2W3sIG_400x400.jpg',
+    twitterLink: 'https://twitter.com/ymotongpoo',
+    githubLink: 'https://github.com/ymotongpoo'
+  },
+  {
+    name: 'micchie',
+    organizationName: '株式会社メルペイ',
+    icon: 'https://pbs.twimg.com/profile_images/1120265745215021056/FN-52cpe_400x400.png',
+    twitterLink: 'https://twitter.com/micchiebear',
+    githubLink: 'https://github.com/mi-bear'
+  },
+  {
+    name: 'luccafort',
+    organizationName: '株式会社マネーフォワード',
+    icon: 'https://pbs.twimg.com/profile_images/877545573787684864/rVHjLVrH_400x400.jpg',
+    twitterLink: 'https://twitter.com/luccafort',
+    githubLink: 'https://github.com/luccafort/'
+  },
+  {
+    name: 'terry',
+    organizationName: '株式会社Showcase Gig',
+    icon: 'https://pbs.twimg.com/profile_images/1580561046615777280/QrNTlXF0_400x400.jpg',
+    twitterLink: 'https://twitter.com/1019hiroya/',
+    githubLink: 'https://github.com/Hiroya3'
+  },
+  {
+    name: 'sivchari',
+    organizationName: 'CyberAgent',
+    icon: 'https://pbs.twimg.com/profile_images/1605935801862606848/wClgAXFu_400x400.jpg',
+    twitterLink: 'https://twitter.com/sivchari',
+    githubLink: 'https://github.com/sivchari'
+  },
+  {
+    name: 'Kiyokawa Taiga',
+    organizationName: '株式会社マネーフォワード',
+    icon: 'https://avatars.githubusercontent.com/u/40013676?v=4',
+    twitterLink: 'https://twitter.com/taigakiyokawa',
+    githubLink: 'https://github.com/taigakiyokawa'
+  },
+  {
+    name: 'sadah',
+    organizationName: '株式会社メルコイン',
+    icon: 'https://sadah.dev/images/icon.jpg',
+    twitterLink: 'https://twitter.com/sada_h',
+    githubLink: 'https://github.com/sadah'
+  },
+  {
+    name: 'Senoue',
+    organizationName: 'Videomarket Co., Ltd.',
+    icon: 'https://avatars.githubusercontent.com/u/3499852?v=4',
+    twitterLink: 'https://twitter.com/senoue',
+    githubLink: 'https://github.com/senoue'
+  },
+  {
+    name: 'goro',
+    organizationName: '株式会社メルコイン',
+    icon: 'https://ca.slack-edge.com/TPU5J9YF7-UPV0X7DDW-afe957bb76f3-512',
+    twitterLink: 'https://twitter.com/awakot_56',
+    githubLink: 'https://github.com/awakot'
+  },
+  {
+    name: 'Shinji NAKAMATSU',
+    organizationName: '株式会社オカキチ',
+    icon: 'https://pbs.twimg.com/profile_images/3411045748/cd6597576c6dea78cf141d9d6829a225_400x400.jpeg',
+    twitterLink: 'https://twitter.com/snaka',
+    githubLink: 'https://github.com/snaka'
+  },
+  {
+    name: 'budougumi0617',
+    organizationName: '株式会社LayerX',
+    icon: 'https://avatars.githubusercontent.com/u/1883265',
+    twitterLink: 'https://twitter.com/budougumi0617',
+    githubLink: 'https://github.com/budougumi0617'
+  },
+  {
+    name: 'Ryuji Iwata',
+    icon: 'https://pbs.twimg.com/profile_images/3392135250/937e2b8360125506e58f3dcb0ccb78fb_400x400.png',
+    twitterLink: 'https://twitter.com/qt_luigi',
+    githubLink: 'https://github.com/qt-luigi',
+    otherLink: 'https://sites.google.com/view/ryuji-iwata-portfolio'
+  },
+  {
+    name: 'mikichin',
+    organizationName: '株式会社メルペイ',
+    icon: 'https://pbs.twimg.com/profile_images/1420563286051876866/-q0VIQhE_400x400.jpg',
+    twitterLink: 'https://twitter.com/chida_miki'
+  },
+  {
+    name: '伊藤真彦',
+    organizationName: '株式会社オカキチ',
+    icon: 'https://pbs.twimg.com/profile_images/1233957245764812800/pi0XtJrr_400x400.jpg',
+    twitterLink: 'https://twitter.com/it_guitar',
+    githubLink: 'https://github.com/maito1201'
+  },
+  {
+    name: 'linjiansi',
+    organizationName: 'からくり株式会社',
+    icon: 'https://avatars.githubusercontent.com/u/55640271?v=4',
+    twitterLink: 'https://twitter.com/linjiansi_dev',
+    githubLink: 'https://github.com/linjiansi'
+  }
+]
