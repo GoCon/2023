2f753ab744dcb0ec13b97d9f375296bc7b113aaa
maito1201 <49754654+maito1201@users.noreply.github.com>
2023-05-25 01:04:29 +0900

    fix: static site generate (#236)

--- src/pages/sessions.tsx
+++ src/pages/sessions.tsx
@@ -1,7 +1,13 @@
+import { GetStaticProps } from 'next'
 import { PageSessions } from 'src/components/pages'
 
 const Index = () => {
   return <PageSessions />
+}
+
+// NOTE: next exportで静的ファイルとして生成するため空のpropsを宣言する
+export const getStaticProps: GetStaticProps = async () => {
+  return { props: {} }
 }
 
 export default Index
