8af4cf5a0604626d8e1a62c4a65bad14c2adf96c
maito1201 <itomasa0915@gmail.com>
2023-05-25 00:57:38 +0900

    fix: static site generate

--- src/pages/floor_guide.tsx
+++ src/pages/floor_guide.tsx
@@ -1,7 +1,14 @@
+import { GetStaticProps } from 'next'
+
 import { PageFloorGuide } from 'src/components/pages'
 
 const Index = () => {
   return <PageFloorGuide />
+}
+
+// NOTE: next exportで静的ファイルとして生成するため空のpropsを宣言する
+export const getStaticProps: GetStaticProps = async () => {
+  return { props: {} }
 }
 
 export default Index
--- src/pages/timetable.tsx
+++ src/pages/timetable.tsx
@@ -1,8 +1,14 @@
+import { GetStaticProps } from 'next'
 import { PageTop } from 'src/components/pages'
 
 const Index = () => {
   /* TODO: 各ページの実装 */
   return <PageTop />
+}
+
+// NOTE: next exportで静的ファイルとして生成するため空のpropsを宣言する
+export const getStaticProps: GetStaticProps = async () => {
+  return { props: {} }
 }
 
 export default Index
