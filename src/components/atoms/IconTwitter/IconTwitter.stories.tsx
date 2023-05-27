f8000fb50b452b40e2a132999b03d3c296728f37
Taiga KIYOKAWA <40013676+taigakiyokawa@users.noreply.github.com>
2023-05-27 19:02:06 +0900

    refactor: Refine Footer (#257)

--- src/components/atoms/IconTwitter/IconTwitter.stories.tsx
+++
@@ -1,9 +0,0 @@
-import type { Meta, StoryObj } from '@storybook/react'
-import { IconTwitter } from '.'
-
-const meta: Meta<typeof IconTwitter> = {
-  component: IconTwitter
-}
-export default meta
-
-export const Default: StoryObj<typeof IconTwitter> = {}
