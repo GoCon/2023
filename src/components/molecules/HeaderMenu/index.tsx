56442d34a248af1a7b345b380eee73940375962e
taigakiyokawa <kiyokawa.taiga@moneyforward.co.jp>
2023-05-28 18:48:12 +0900

    chore: Change link to timetable to the internal page

--- src/components/molecules/HeaderMenu/index.tsx
+++ src/components/molecules/HeaderMenu/index.tsx
@@ -16,34 +16,18 @@ export const HeaderMenu = ({ menuList, itemColor, itemBehaviorStyles }: HeaderMe
     <Box sx={{ display: 'flex', gap: '8px', margin: '0 24px 0 auto' }}>
       {menuList.map((list, i) => {
         return list.href ? (
-          list.openNewTab ? (
-            // TODO(taigakiyokawa): Revert using `next/link` when pages have implemented.
-            <a href={list.href} target="_blank" rel="noreferrer" key={i}>
-              <Typography
-                sx={{
-                  borderBottom: router.pathname === list.href ? '3px solid' : '',
-                  color: itemColor.default,
-                  p: '4px 8px',
-                  ...itemBehaviorStyles
-                }}
-              >
-                {list.label}
-              </Typography>
-            </a>
-          ) : (
-            <Link href={list.href} key={i}>
-              <Typography
-                sx={{
-                  borderBottom: router.pathname === list.href ? '3px solid' : '',
-                  color: itemColor.default,
-                  p: '4px 8px',
-                  ...itemBehaviorStyles
-                }}
-              >
-                {list.label}
-              </Typography>
-            </Link>
-          )
+          <Link href={list.href} key={i}>
+            <Typography
+              sx={{
+                borderBottom: router.pathname === list.href ? '3px solid' : '',
+                color: itemColor.default,
+                p: '4px 8px',
+                ...itemBehaviorStyles
+              }}
+            >
+              {list.label}
+            </Typography>
+          </Link>
         ) : (
           <Typography
             onClick={list.onClick}
