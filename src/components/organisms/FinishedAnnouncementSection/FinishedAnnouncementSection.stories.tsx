9707b4a18b324ecdad50cf350a27ad0845cff48a
Taiga KIYOKAWA <40013676+taigakiyokawa@users.noreply.github.com>
2023-06-03 08:48:50 +0900

    feat: Add announcement for the conference has been finished (#307)

---
+++ src/components/organisms/FinishedAnnouncementSection/FinishedAnnouncementSection.stories.tsx
@@ -0,0 +1,9 @@
+import type { Meta, StoryObj } from '@storybook/react'
+import { FinishedAnnouncementSection } from '.'
+
+const meta: Meta<typeof FinishedAnnouncementSection> = {
+  component: FinishedAnnouncementSection
+}
+export default meta
+
+export const Default: StoryObj<typeof FinishedAnnouncementSection> = {}
